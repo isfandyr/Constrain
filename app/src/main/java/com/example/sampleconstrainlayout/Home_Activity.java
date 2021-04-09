11406123735b032fd8fd508579c75aa4ff1e27a8
Muhammad Adrianto <isfan.diar123@gmail.com>
2021-04-09 22:59:49 +0700

    Acticity 5

---
+++ app/src/main/java/com/example/sampleconstrainlayout/Home_Activity.java
@@ -0,0 +1,109 @@
+package com.example.sampleconstrainlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Home_Activity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    //Deklarasi variabel dengan jenis data listview
+    private ListView list;
+
+    //Memanggil class ListviewAdapter dan diinisiasi menjadi variabel adapter
+    private ListViewAdapter adapter;
+
+    //Deklarasi array untuk menyimpan nama
+    String[] listNama;
+
+    //Memanggil class CmassNama
+    public static ArrayList<ClassNama> classNamaArrayList = new ArrayList<ClassNama>();
+
+    //Membuat objek bundle
+    Bundle bundle = new Bundle();
+
+    //Membuat objek intent
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_);
+
+        //Menyimpan nama didalam array ListNama
+        listNama = new String[]{"Inayah", "Ilham", "Eris",
+                "Fikri", "Maul", "Intan", "Vina", "Gita",
+                "Vian", "Lutfi"};
+
+        list = findViewById(R.id.listKontak);
+
+        //membuat objek dari class ClassNama menjadi arraylist
+        classNamaArrayList = new ArrayList<>();
+
+        //Membaca seluruh data pada array ListNama
+        for (int i = 0; i < listNama.length; i++)
+        {
+            //Membuat objek class nama da
+            ClassNama classNama = new ClassNama(listNama[i]);
+            // Binds strings ke array
+            classNamaArrayList.add(classNama);
+        }
+
+        // Membuat objek dari ListViewAdapter
+        adapter = new ListViewAdapter(this);
+
+        // Binds Adapter ke ListView
+        list.setAdapter(adapter);
+
+        //Membuat event dari List onclick
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Deklarasi variabel nama yang berisi item yang diklik
+                String nama = classNamaArrayList.get(position).getName();
+
+                //memasukkan value dari variabel nama dengan kunci "a"
+                // dan dimasukkan kedalam bundle
+                bundle.putString("a",nama.trim());
+
+                //Membuat objek popup menu
+                PopupMenu pm = new PopupMenu(getApplicationContext(),view);
+
+                // Membuat event untuk popup menu ketika dipilih
+                pm.setOnMenuItemClickListener(Home_Activity.this);
+
+                //Menampilkan popup menu dari Layout menu
+                pm.inflate(R.menu.popup_menu);
+
+                //Menampilkan popup menu
+                pm.show();
+            }
+        });
+    }
+
+    //event yang terjadi ketika menu dipilih
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.mnview:
+                intent = new Intent(getApplicationContext(),ActivityLihatData.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+            case R.id.mnedit:
+                Toast.makeText(getApplicationContext(),"Ini untuk edit kontak",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/sampleconstrainlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstrainlayout/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sampleconstrainlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,33 +46,59 @@ public class MainActivity extends AppCompatActivity {
                 String email = "isfan.diar123@gmail.com";
                 String pass = "777";
 
-                if (nama.equals(email) && password.equals(pass))
-                {
+                if (nama.equals(email) && password.equals(pass)) {
                     //membuat variabel toast dan menampilkan pesan
                     Toast t = Toast.makeText(getApplicationContext(),
                             "Login Sukses", Toast.LENGTH_LONG);
                     //menampilkan toast
                     t.show();
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("a", nama.trim());
+                    bundle.putString("b", password.trim());
+
+                    Intent intent = new Intent(getApplicationContext(), Home_Activity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
                 }
                 else if (!nama.equals(email) && password.equals(pass))
                 {
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "Email Salah", Toast.LENGTH_LONG);
+                            "Email Salah!", Toast.LENGTH_LONG);
                     t.show();
                 }
                 else if (nama.equals(email) && !password.equals(pass))
                 {
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "Password Salah", Toast.LENGTH_LONG);
+                            "Password Salah!", Toast.LENGTH_LONG);
                     t.show();
                 }
-                else
-                {
+                else {
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "Email dan Password Salah", Toast.LENGTH_LONG);
+                            "Email dan Password Salah!", Toast.LENGTH_LONG);
                     t.show();
                 }
             }
+
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //Method untuk memanggil actvity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
